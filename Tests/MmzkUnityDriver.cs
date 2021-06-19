bf97cef15e480898df01bee9c807777d9133c17f
uisawara <yuichi.ogasawara.1121@gmail.com>
2021-06-19 20:00:42 +0900

    Initial Commit

---
+++ Tests/MmzkUnityDriver.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEditor.Build.Content;
+using UnityEditor.SceneManagement;
+
+namespace Tests
+{
+    public interface IMmzkUnityDriver
+    {
+        bool TakeScreenshotsAutomatically { get; set; }
+        float DefaultTimeoutSeconds { get; set; }
+        string ScreenshotBasePathName { get; set; }
+        IEnumerator LoadScene(string sceneName);
+        IEnumerator Wait(string gameObjectName);
+        IEnumerator Wait(string gameObjectName, float timeoutSeconds);
+        IEnumerator Click(string buttonObjectName);
+        IEnumerator Click(string buttonObjectName, float timeoutSeconds);
+        IEnumerator TakeScreenshot(string imageFileName);
+        IEnumerator TakeScreenshot(string imageFileName, float timeoutSeconds);
+    }
+    
+    public class MmzkUnityDriver : IMmzkUnityDriver
+    {
+        public bool TakeScreenshotsAutomatically { get; set; } = false;
+        public float DefaultTimeoutSeconds { get; set; } = 5f;
+
+        public string ScreenshotBasePathName { get; set; } = "results/screenshots/";
+
+        private int _counter;
+
+        public IEnumerator LoadScene(string sceneName)
+        {
+            EditorSceneManager.LoadScene(sceneName);
+            yield return null;
+        }
+
+        public IEnumerator Wait(string gameObjectName)
+        {
+            if (TakeScreenshotsAutomatically)
+            {
+                yield return TakeScreenshot($"{ScreenshotBasePathName}{_counter++}.png", DefaultTimeoutSeconds);
+            }
+            yield return TestUtil.Wait(gameObjectName, DefaultTimeoutSeconds);
+        }
+        
+        public IEnumerator Wait(string gameObjectName, float timeoutSeconds)
+        {
+            if (TakeScreenshotsAutomatically)
+            {
+                yield return TakeScreenshot($"{ScreenshotBasePathName}{_counter++}.png", timeoutSeconds);
+            }
+            yield return TestUtil.Wait(gameObjectName, timeoutSeconds);
+        }
+
+        public IEnumerator Click(string buttonObjectName)
+        {
+            if (TakeScreenshotsAutomatically)
+            {
+                yield return TakeScreenshot(DefaultTimeoutSeconds);
+            }
+            yield return TestUtil.Click(buttonObjectName, DefaultTimeoutSeconds);
+        }
+
+        public IEnumerator Click(string buttonObjectName, float timeoutSeconds)
+        {
+            if (TakeScreenshotsAutomatically)
+            {
+                yield return TakeScreenshot(timeoutSeconds);
+            }
+            yield return TestUtil.Click(buttonObjectName, timeoutSeconds);
+        }
+        
+        public IEnumerator TakeScreenshot(string imageFileName)
+        {
+            yield return TakeScreenshot(imageFileName, DefaultTimeoutSeconds);
+        }
+
+        public IEnumerator TakeScreenshot(string imageFileName, float timeoutSeconds)
+        {
+            yield return TestUtil.TakeScreenshot(imageFileName, timeoutSeconds);
+        }
+
+        private IEnumerator TakeScreenshot(float timeoutSeconds)
+        {
+            return TakeScreenshot($"{ScreenshotBasePathName}{_counter++}.png", timeoutSeconds);
+        }
+    }
+}
---
+++ Tests/TestUtils.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tests
+{
+    public static class TestUtil
+    {
+        public static IEnumerator TakeScreenshot(string imageFileName, float timeoutSeconds)
+        {
+            Debug.Log($"[TakeScreenshot] {imageFileName}");
+
+            var imageFilePath = Path.GetDirectoryName(imageFileName);
+            if (!Directory.Exists(imageFilePath))
+            {
+                Directory.CreateDirectory(imageFilePath);
+            }
+            
+            if (File.Exists(imageFileName))
+            {
+                File.Delete(imageFileName);
+            }
+            
+            ScreenCapture.CaptureScreenshot(imageFileName);
+
+            var startTime = Time.time;
+            float timespan = 0f;
+            while (File.Exists(imageFileName) == false && (timespan = Time.time - startTime) < timeoutSeconds)
+            {
+                yield return null;
+            }
+            
+            if (timespan >= timeoutSeconds)
+            {
+                throw new TimeoutException($"TakeScreenshot {imageFileName} timedout");
+            }
+        }
+
+        public static IEnumerator Wait(string gameObjectName, float timeoutSeconds)
+        {
+            Debug.Log($"[Wait] {gameObjectName}");
+            var startTime = Time.time;
+            float timespan = 0f;
+            yield return new WaitWhile(() => GameObject.Find(gameObjectName) == null && (timespan = Time.time - startTime) < timeoutSeconds);
+            if (timespan >= timeoutSeconds)
+            {
+                throw new TimeoutException($"Wait {gameObjectName} timedout");
+            }
+        }
+
+        public static IEnumerator Click(string buttonObjectName, float timeoutSeconds)
+        {
+            Debug.Log($"[Click] {buttonObjectName}");
+            GameObject gameObject = null;
+            var startTime = Time.time;
+            float timespan = 0f;
+            yield return new WaitWhile(() => (gameObject = GameObject.Find(buttonObjectName)) == null && (timespan = Time.time - startTime) < timeoutSeconds);
+            if (timespan >= timeoutSeconds)
+            {
+                throw new TimeoutException($"Click {buttonObjectName} timedout");
+            }
+            var button = gameObject.GetComponent<Button>();
+            if (button == null)
+            {
+                throw new NullReferenceException($"{buttonObjectName} has not Button component");
+            }
+            
+            button.onClick.Invoke();
+        }
+    }
+}
